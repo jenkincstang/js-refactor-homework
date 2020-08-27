8ab9d27e6251107716fe0bd21d404763ecb1d996
Jenkin Tang <jenkincs@163.com>
2020-08-28 00:35:51 +0800

    [js-refactor-homework][Jenkin]test: test voyageProfitFactor test voyage.zone = china and hasChina(history)==TRUE and history.length = 10

--- test/rankTest.js
+++ test/rankTest.js
@@ -344,3 +344,53 @@ rankTest('captainHistoryRisk test voyage.zone = east-indies and history.length =
   //then
   t.is(result, 2);
 })
+
+rankTest('voyageProfitFactor test voyage.zone = china and hasChina(history)==TRUE and history.length = 10', t => {
+  //given
+  const voyage = {
+    zone: 'china',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+    {
+      zone: 'west-africa1',
+      profit: 7,
+    },{
+      zone: 'west-africa2',
+      profit: 7,
+    },
+    {
+      zone: 'west-africa1',
+      profit: 7,
+    },{
+      zone: 'west-africa2',
+      profit: 7,
+    },
+    {
+      zone: 'west-africa1',
+      profit: 7,
+    },{
+      zone: 'west-africa2',
+      profit: 7,
+    },
+  ];
+  //when
+  const result = voyageProfitFactor (voyage, history)
+  //then
+  t.is(result, 6);
+})
+
