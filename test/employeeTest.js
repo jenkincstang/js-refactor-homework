7b8a61558d9f0019b995fc404aaaa6ad61d9c152
Jenkin Tang <jenkincs@163.com>
2020-08-28 23:55:42 +0800

    [js-refactor-homework][jenkin] test: add employeeTest : test constructor() output when throw error

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -1,7 +1,16 @@
 const employeeTest = require('ava');
 const { Employee } = require('../src/employee');
 
-employeeTest("employeeTest test toString() output when no throw error",t => {
+employeeTest("employeeTest : test toString() output when no throw error",t => {
   let employee = new Employee("Jenkin","engineer");
   t.is(employee.toString(),"Jenkin (engineer)");
+})
+
+employeeTest("employeeTest : test constructor() output when throw error",t => {
+  try{
+    let employee = new Employee("Jenkin","engineer1");
+    t.fail();
+  }catch (e){
+    t.is(e.message,"Employee cannot be of type engineer1")
+  }
 })
