e14f7d747e3a393ce239eb2951dbcb0b55ed88d1
Jenkin Tang <jenkincs@163.com>
2020-08-29 00:24:03 +0800

    [js-refactor-homework][jenkin] test: add deliveryDateTest: test deliveryState=TEST and isRush is false

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -35,3 +35,10 @@ deliveryDateTest("deliveryDateTest: test deliveryState=ME and isRush is false",t
       }}};
   t.is(deliveryDate(anOrder,false),5)
 })
+
+deliveryDateTest("deliveryDateTest: test deliveryState=TEST and isRush is false",t => {
+  let anOrder = {deliveryState:'TEST', placedOn:{plusDays:function (deliveryTime) {
+        return deliveryTime;
+      }}};
+  t.is(deliveryDate(anOrder,false),6)
+})
