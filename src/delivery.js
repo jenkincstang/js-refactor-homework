e5e9a16c73a7a0f24643da3bbd4cb205c50f4bbd
Jenkin Tang <jenkincs@163.com>
2020-08-29 00:49:27 +0800

    [js-refactor-homework][jenkin]refactor: replace if-else statement to map

--- src/delivery.js
+++ src/delivery.js
@@ -1,19 +1,14 @@
 function deliveryDate (anOrder, isRush) {
+  let deliveryStateMapWithIsRushTrue = new Map();
+  deliveryStateMapWithIsRushTrue.set('MA',1);
+  deliveryStateMapWithIsRushTrue.set('CT',1);
+  deliveryStateMapWithIsRushTrue.set('NY',2);
+  deliveryStateMapWithIsRushTrue.set('NH',2);
   if (isRush) {
     let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 1;
-    }
-    else if ([
-      'NY',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else {
+    if (deliveryStateMapWithIsRushTrue.has(anOrder.deliveryState)){
+      deliveryTime = deliveryStateMapWithIsRushTrue.get(anOrder.deliveryState);
+    }else {
       deliveryTime = 3;
     }
     return anOrder.placedOn.plusDays(1 + deliveryTime);
